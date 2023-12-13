f5be3d1a4ffcd4abab456d603a7f7fee8fabc0ce
vic1707 <28602203+vic1707@users.noreply.github.com>
2023-12-13 14:04:08 +0100

    argument parsing + tests

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -1,3 +1,27 @@
-describe('App', () => {
-    /* ... */
+const { parseCLIArguments } = require('../app');
+
+describe('CLI Args parsing', () => {
+    it('should parse one argument', () => {
+        process.argv = ['node', 'app.js', '--filter=ry'];
+        const expected = new Map([['filter', ['ry']]]);
+        expect(parseCLIArguments()).toEqual(expected);
+    });
+
+    it('should parse multiple arguments', () => {
+        process.argv = ['node', 'app.js', '--filter=ry', '--filter=uc'];
+        const expected = new Map([['filter', ['ry', 'uc']]]);
+        expect(parseCLIArguments()).toEqual(expected);
+    });
+
+    it('should ignore arguments without "="', () => {
+        process.argv = ['node', 'app.js', '--filter'];
+        const expected = new Map();
+        expect(parseCLIArguments()).toEqual(expected);
+    });
+
+    it('should ignore arguments without "--"', () => {
+        process.argv = ['node', 'app.js', 'filter=ry'];
+        const expected = new Map();
+        expect(parseCLIArguments()).toEqual(expected);
+    });
 });
--- app.js
+++ app.js
@@ -6,3 +6,42 @@
  * @type {Country[]}
  */
 const { data } = require('./data.js');
+
+/**
+ * @description Parse CLI arguments. Arguments are expected to be in the form of:
+ * --argumentName=argumentValue
+ * @returns {Map<String, Array<String>>} Map of arguments.
+ */
+function parseCLIArguments() {
+    const args = process.argv.slice(2);
+    /**
+     * @type {Map<String, Array<String>>}
+     */
+    const arguments_map = new Map();
+
+    args
+        // Filter out arguments that do not start with '--' and do not contain '='.
+        .filter((arg) => arg.startsWith('--') && arg.includes('='))
+        // Extract argument name and value. Add them to the map.
+        .forEach((arg) => {
+            const [key, value] = arg.split('=');
+            const argumentName = key.replace('--', '');
+            const values = arguments_map.get(argumentName) || [];
+
+            values.push(value);
+            arguments_map.set(argumentName, values);
+        });
+
+    return arguments_map;
+}
+
+
+
+
+
+
+
+// Used for testing purposes
+module.exports = {
+    parseCLIArguments,
+};
